425eb9fe2f374b37949b4628792212c976bf85f9
AnKhudyakov <andrew_job@list.ru>
2023-07-19 22:20:14 +0400

    fix: refetch items then remove collection

--- client/src/features/item/ItemsList.jsx
+++ client/src/features/item/ItemsList.jsx
@@ -2,17 +2,27 @@ import {
   useGetItemsByCollectionQuery,
   useGetItemsQuery,
 } from "@/app/services/item";
+import { selectIsUpdateItems } from "@/features/collection/collectionSlice";
 import Item from "@/features/item/Item";
 import { Box, Typography } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 
 const ItemsList = ({ collectionId, variant }) => {
   const { t } = useTranslation("translation", { keyPrefix: "collection" });
-  const { data: items, isLoading } =
-    variant === "lastItems"
-      ? useGetItemsQuery({ limit: 5, sort_by: "createdAt", sort_order: "desc" })
-      : useGetItemsByCollectionQuery(collectionId);
+  const isUpdateItems = useSelector(selectIsUpdateItems);
+  const {
+    data: items,
+    isLoading,
+    refetch,
+  } = variant === "lastItems"
+    ? useGetItemsQuery({ limit: 5, sort_by: "createdAt", sort_order: "desc" })
+    : useGetItemsByCollectionQuery(collectionId);
+  useEffect(() => {
+    refetch();
+  }, [isUpdateItems]);
   return (
     <Box display="flex" flexDirection="column" gap={2} mt={1}>
       {isLoading ? (
