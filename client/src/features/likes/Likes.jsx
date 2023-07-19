2816c4eca6efb23b6f9fb5b0f0deb1f25b19b002
AnKhudyakov <andrew_job@list.ru>
2023-07-19 23:08:43 +0400

    fix: add loader then add like

--- client/src/features/likes/Likes.jsx
+++ client/src/features/likes/Likes.jsx
@@ -1,21 +1,25 @@
 import { useAddLikeMutation } from "@/app/services/item";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, CircularProgress, IconButton, Typography, useMediaQuery } from "@mui/material";
 
 const Likes = ({ user, item }) => {
-  const [addLike, { isLoading: isUpdating }] = useAddLikeMutation();
+  const [addLike, { isLoading }] = useAddLikeMutation();
+  const isNonMobile = useMediaQuery("(min-width:740px)");
   return (
-    <Box display="flex" alignItems="center" px={2} height={"50%"}>
-      <Typography variant="body2" color="text.secondary" textAlign="center">
-        {item?.likes.length}
-      </Typography>
-      <IconButton
-        aria-label="edit"
-        onClick={() => addLike({ id: item._id, like: user._id }).unwrap()}
-        disabled={item?.likes.includes(user?._id)||!user}
-      >
-        <FavoriteIcon />
-      </IconButton>
+    <Box display={"flex"} flexDirection={isNonMobile?"column":"row"} alignItems={"center"}>
+      <Box display="flex" alignItems="center" px={2} height={"50%"}>
+        <Typography variant="body2" color="text.secondary" textAlign="center">
+          {item?.likes.length}
+        </Typography>
+        <IconButton
+          aria-label="edit"
+          onClick={() => addLike({ id: item._id, like: user._id }).unwrap()}
+          disabled={item?.likes.includes(user?._id) || !user}
+        >
+          <FavoriteIcon />
+        </IconButton>
+      </Box>
+      {isLoading && <CircularProgress size={20} />}
     </Box>
   );
 };
